57f0f7a3e3e943f14bdcb5373dd04a5726742be5
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-05-08 18:28:59 +0100

    Add styling and structure

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+const toggleBtn = document.querySelector(".toggle-button")[0];
+const navbarLinks = document.querySelector(".nav__links")[0];
 
+toggleBtn.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
