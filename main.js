3479309ea650fd4bb022c6af6b556e7e2e3616d3
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-05-08 18:49:47 +0100

    Add sticky navigation

--- main.js
+++ main.js
@@ -1,6 +1,55 @@
-const toggleBtn = document.querySelector(".toggle-button")[0];
-const navbarLinks = document.querySelector(".nav__links")[0];
+// const toggleBtn = document.querySelector(".toggle-button")[0];
+// const navbarLinks = document.querySelector(".nav__links")[0];
 
-toggleBtn.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
+// toggleBtn.addEventListener("click", () => {
+//   navbarLinks.classList.toggle("active");
+// });
+
+/** Menu Fade Animation */
+const nav = document.querySelector(".nav");
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((sibling) => {
+      if (sibling !== link) sibling.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+/** Page Navigaton */
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy - check if target has the matching class
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
+
+/**Sticky Navigation  */
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
